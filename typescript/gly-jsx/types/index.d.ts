a85e0fab2c8c0c015863bf7dc22f25f55a89e824
DornellesTV <rodrigo.dornelles.poa@gmail.com>
2025-12-06 21:57:42 -0300

    feat: gly-jsx 0.2.2

--- typescript/gly-jsx/types/index.d.ts
+++ typescript/gly-jsx/types/index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @version 0.2.1
+ * @version 0.2.2
  */
 
 declare namespace JSX {
@@ -7,8 +7,8 @@ declare namespace JSX {
   }
 
   interface IntrinsicElements {
-    grid: { class: string, span?: number, style?: string, dir?: 1 | 0, children?: JSX.Element | Array<JSX.Element>};
-    item: ({ span: number} | {style: string}) & {children: JSX.Element}
+    grid: { class: string, span?: number, offset?: number, style?: string, dir?: 1 | 0, children?: JSX.Element | Array<JSX.Element>};
+    item: ({ span: number} | {offset: number} | {style: string}) & {children: JSX.Element}
     node: { children?: never, [key: string]: unknown;};
     style: { class: string, children?: never} | {[key: string]: number};
   }
