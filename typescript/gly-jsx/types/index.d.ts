a990914cebcaf0d0fa4f8608e478205b8dba39ee
DornellesTV <rodrigo.dornelles.poa@gmail.com>
2026-01-21 08:21:25 -0300

    feat: update gly-jsx to 0.3.X

--- typescript/gly-jsx/types/index.d.ts
+++ typescript/gly-jsx/types/index.d.ts
@@ -1,18 +1,21 @@
 /**
- * @version 0.2.3
+ * @version 0.3.0
  */
 
 declare namespace JSX {
-  interface Element extends Record<string, Function> {
-  }
+  const __gly_jsx: unique symbol;
+
+  type Element = {
+    readonly [__gly_jsx]: keyof IntrinsicElements;
+  };
 
   interface IntrinsicElements {
-    grid: { class: string, span?: number, offset?: number, after?: number, style?: string, dir?: 1 | 0, children?: JSX.Element | Array<JSX.Element>};
-    item: ({ span: number} | {offset: number} | {after: number} | {style: string}) & {children: JSX.Element}
-    node: { children?: never, [key: string]: unknown;};
-    style: { class: string, children?: never} | {[key: string]: number};
+    grid: { class: string, span?: number, offset?: number, style?: string, dir?: 1 | 0, children?: JSX.Element | Array<JSX.Element> };
+    item: ({ span: number } | { offset: number } | { style: string }) & { children: JSX.Element }
+    node: { children?: JSX.Element | Array<JSX.Element> } | { [key: string]: Function; };
+    style: { class: string, children?: never } | { [key: string]: number };
   }
-  
+
   interface ElementChildrenAttribute {
     children: {};
   }
